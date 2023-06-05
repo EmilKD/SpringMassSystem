472163fdd6b483aef8422098422dcbf877c5bb2a
Emil Keshishian <42214323+EmilKD@users.noreply.github.com>
2023-06-05 18:58:48 +0430

    Texture Moved into the Shader Class

--- Graphics.cpp
+++ Graphics.cpp
@@ -1,9 +1,8 @@
 #include"Graphics.h"
 #include"Shader.h"
-#define STB_IMAGE_IMPLEMENTATION
-#include"stb_image.h"
-
-//#include"TextureGen.h"
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
 
 // Prototype Functinos (Callbacks)
 static float g_xpos, g_ypos;
@@ -171,33 +170,8 @@ int main()
 
 	// Texture Generation --------------------------------------------------------------------------------------------
 	
-	//MainShader.CreateTexture(". / Textures / Spring.png", "png");
+	MainShader.CreateTexture(". / Textures / Spring.png", "png");
 	
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	unsigned int texture;
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
-	// set the texture wrapping/filtering options (on the currently bound texture object)
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// load and generate the texture
-	int width, height, nrChannels;
-	unsigned char* data = stbi_load("./Textures/Spring.png", &width, &height, &nrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
-	stbi_image_free(data);
-	glBindTexture(GL_TEXTURE_2D, texture);
 	
 	// Variables and Objects declaration------------------------------------------------------------------------------
 	vector<float> vertices =
